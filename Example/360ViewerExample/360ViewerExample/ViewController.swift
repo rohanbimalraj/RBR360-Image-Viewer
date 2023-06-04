b6b50b933cd8d1acacb9ec8d87b0747df597a8d6
Rohan Bimal Raj <rohanbimalraj@gmail.com>
2023-06-04 15:51:44 +0530

    Implemented photo capture.

--- Example/360ViewerExample/360ViewerExample/ViewController.swift
+++ Example/360ViewerExample/360ViewerExample/ViewController.swift
@@ -24,6 +24,10 @@ class ViewController: UIViewController {
         rbrView.loadImage(with: url!, camera: .gesture)
         
         NotificationCenter.default.addObserver(self, selector: #selector(itemRemoved), name: .deletedModelName, object: nil)
+        
+        //Please add photo library usage description in info pList to use image capture fuctionality.
+        NotificationCenter.default.addObserver(self, selector: #selector(imageSavedSuccessfully), name: .imageSaveSuccess, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(imageSavedWithError), name: .imageSaveFailure, object: nil)
     }
 
     @IBAction func addButtonACtion(_ sender: Any) {
@@ -35,6 +39,21 @@ class ViewController: UIViewController {
             print("Removed item:", name)
         }
     }
-
+    
+    @objc func imageSavedSuccessfully() {
+        let alert = UIAlertController(title: "Success", message: "Captured image is saved successfully in Photos", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel))
+        self.present(alert, animated: true)
+    }
+    
+    @objc func imageSavedWithError() {
+        let alert = UIAlertController(title: "Failure", message: "Unfortunately captured image couldn't be saved", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel))
+        self.present(alert, animated: true)
+    }
+    
+    @IBAction func captureButtonActiob(_ sender: Any) {
+        rbrView.captureScreen()
+    }
 }
 
